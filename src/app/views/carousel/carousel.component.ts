c0b364f0651db5fdc03368c59672194572a47a42
julio <juliocesar.melegari@gmail.com>
2025-08-02 21:04:43 -0300

    add template, estilos, projeto finalizado

--- src/app/views/carousel/carousel.component.ts
+++ src/app/views/carousel/carousel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ImdbService } from '../../services/imdb.service';
+import { MatCardHeader, MatCardTitle, MatCardContent, MatCardSubtitle } from '@angular/material/card';
+import {  MatCard } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardSubtitle, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent implements OnInit {
 
   public movies: any = [];
+  currentMovie = 0;
 
   constructor(private imdbService: ImdbService){}
 
@@ -30,6 +36,16 @@ export class CarouselComponent implements OnInit {
       });
     });
     console.log(this.movies);
+  }
+
+  onPreviousClick(){
+    const previous = this.currentMovie - 1;
+    this.currentMovie = previous < 0 ? this.movies.length - 1 : previous;
+  }
+
+  onNextClick(){
+    const next = this.currentMovie + 1;
+    this.currentMovie = next === this.movies.length ? 0 : next;
   }
 
 }
